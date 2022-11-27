a8f24660c445bea90457fa9a49bf5dc0dbe527b1
Hasan Berk <hasanberk50@gmail.com>
2022-11-27 14:35:13 +0000

    Using react-query to manage the browser-based cache.

--- src/index.js
+++ src/index.js
@@ -8,20 +8,35 @@ import {Link} from 'react-router-dom';
 import MovieReviewPage from "./pages/movieReviewPage";
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import SiteHeader from './components/siteHeader';
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 360000,
+        refetchInterval: 360000, 
+        refetchOnWindowFocus: false
+      },
+    },
+  });
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <SiteHeader />      {/* New Header  */}
-      <Routes>
-        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
-        <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
-        <Route path="/movies/:id" element={<MoviePage />} />
-        <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={ <Navigate to="/" /> } />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+        <SiteHeader />      {/* New Header  */}
+        <Routes>
+            <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+            <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
+            <Route path="/movies/:id" element={<MoviePage />} />
+            <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={ <Navigate to="/" /> } />
+        </Routes>
+        </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -3,16 +3,24 @@ import { useParams } from 'react-router-dom';
 import MovieDetails from "../components/movieDetails/";
 import PageTemplate from "../components/templateMoviePage";
 import { getMovie } from "../api/tmdb-api";
+import { useQuery } from "react-query";
+import Spinner from '../components/spinner';
 
-const MoviePage = (props) => {
-  const { id } = useParams();
-  const [movie, setMovie] = useState(null);
-
-  useEffect(() => {
-    getMovie(id).then((movie) => {
-      setMovie(movie);
-    });
-  }, [id]);
+const MovieDetailsPage = (props) => {
+    const { id } = useParams();
+  
+    const { data: movie, error, isLoading, isError } = useQuery(
+      ["movie", { id: id }],
+      getMovie
+    );
+  
+    if (isLoading) {
+      return <Spinner />;
+    }
+  
+    if (isError) {
+      return <h1>{error.message}</h1>;
+    }
 
   return (
     <>
@@ -29,4 +37,4 @@ const MoviePage = (props) => {
   );
 };
 
-export default MoviePage;
+export default MovieDetailsPage;
